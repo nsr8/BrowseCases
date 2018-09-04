9c2f22979c9ed576516f4e04fc6659ac7fa20016
nsr8 <31838903+nsr8@users.noreply.github.com>
2018-09-05 00:27:35 +0530

    Implemented list view
    implemented list view
    removed the test button

--- app/src/main/java/com/zuhair/nasri/finalcases/ListActivity.java
+++ app/src/main/java/com/zuhair/nasri/finalcases/ListActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -13,8 +19,12 @@ public class ListActivity extends AppCompatActivity {
     TextView   displaySection;
 
 
-    //test button
-    Button testBtn;
+
+
+        // TODO: check functionlaity of array adapter
+        ArrayList<String> movies;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +36,47 @@ public class ListActivity extends AppCompatActivity {
         displaySection.setText(getIntent().getStringExtra("SECTION"));
 
 
-        //test button
-        testBtn= findViewById(R.id.testBtn);
-        testBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent testIntent = new Intent(ListActivity.this, ScrollingActivity.class);
-                startActivity(testIntent);
+
+
+
+        // Get the reference of movies
+        ListView moviesList=(ListView)findViewById(R.id.displayList);
+
+        movies = new ArrayList<String>();
+        getmovies();
+
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, movies);
+        // Set The Adapter
+        moviesList.setAdapter(arrayAdapter);
+
+        // register onClickListener to handle click events on each item
+        moviesList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3)
+            {
+                String selectedmovie=movies.get(position);
+                Toast.makeText(getApplicationContext(), "Movie Selected : "+selectedmovie,   Toast.LENGTH_LONG).show();
             }
         });
+
+
+
+
+
     }
+   void getmovies()
+    {
+        movies.add("X-Men");
+        movies.add("IRONMAN");
+        movies.add("SPIDY");
+        movies.add("NARNIA");
+        movies.add("LIONKING");
+        movies.add("AVENGERS");
+    }
+
+
+
 }
